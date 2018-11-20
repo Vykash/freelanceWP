e353e4286d8f142870a3c079347aca0380a1a326
Vykash <banci.lorenzo@gmail.com>
2018-11-20 17:11:24 +0100

    Fix ul duplicate

--- header.php
+++ header.php
@@ -35,8 +35,8 @@
           <i class="fas fa-bars"></i>
         </button>
             <div class="collapse navbar-collapse" id="navbarResponsive">
-                <ul class="navbar-nav ml-auto">
-                    <?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?>
+                 <?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?>
+                <!--<ul class="navbar-nav ml-auto">-->
                     <!--<li class="menu-item nav-item mx-0 mx-lg-1">
                         <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#portfolio">Portfolio</a>
                     </li>
@@ -46,7 +46,7 @@
                     <li class="menu-item nav-item mx-0 mx-lg-1">
                         <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#contact">Contact</a>
                     </li>-->
-                </ul>
+                <!--</ul>-->
             </div>
         </div>
     </nav>
